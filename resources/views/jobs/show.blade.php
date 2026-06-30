0aeb3b4feefa05c5b04cd033e3ca615c92846ae1
valerydochev <valerisdochev@gmail.com>
2026-06-30 17:05:59 +0300

    Add update, and delete functionals to the jobs

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,10 +3,14 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
+
+    <div class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </div>
 
 </x-layout>
